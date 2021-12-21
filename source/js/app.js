27fbf8e9337f0614280f7865fcc9e9a635f7f12f
Alexander <kaamosdao@gmail.com>
2021-12-21 18:33:10 +0300

    Декоративные элементы главной страницы

---
+++ source/js/app.js
@@ -0,0 +1,14 @@
+const app = () => {
+  const mainNavButton = document.querySelector(".main-nav__toggle");
+  const mainNav = document.querySelector(".main-nav");
+
+  mainNav.classList.remove("main-nav--nojs");
+
+  mainNavButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    mainNav.classList.toggle("main-nav--closed");
+    mainNav.classList.toggle("main-nav--opened");
+  });
+};
+
+app();
