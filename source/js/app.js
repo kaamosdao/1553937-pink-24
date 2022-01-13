9c2c38f27f5fbfd13b04fb5c0691704723833161
Alexander <kaamosdao@gmail.com>
2022-01-13 18:03:45 +0300

    скорректирован gulpfile для build, dev

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,7 @@
 const app = () => {
   const mainNavButton = document.querySelector(".main-nav__toggle");
   const mainNav = document.querySelector(".main-nav");
-
   mainNav.classList.remove("main-nav--nojs");
-
   mainNavButton.addEventListener("click", (e) => {
     e.preventDefault();
     mainNav.classList.toggle("main-nav--closed");
